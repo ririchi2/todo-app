3d728e6db583f797b62af64a3300a86c65e07b9c
richipineda <richipineda2@gmail.com>
2022-07-07 11:47:55 +0100

    pwa ready 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,25 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ registerType: 'autoUpdate',
+    manifest: {
+      name: 'todoApp',
+      short_name: 'MyToDoApp',
+      description: '',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  }),
   ]
 })
